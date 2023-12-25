3a4a620600c3b93408ef1f7bb7d93bd8a63b4a34
liaoyi <2417276459@qq.com>
2023-12-25 11:14:27 +0800

    feat: 新增富文本页面 :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,8 @@ import 'virtual:uno.css';
 import '@unocss/reset/tailwind.css';
 import '@/styles/index.css';
 import 'virtual:svg-icons-register';
-import 'simplebar-react/dist/simplebar.min.css';
+import 'react-quill/dist/quill.snow.css'; /** 富文本 */
+import 'simplebar-react/dist/simplebar.min.css'; /** 滚动条 */
 
 import App from '@/App';
 import '@/locales/i18n';
