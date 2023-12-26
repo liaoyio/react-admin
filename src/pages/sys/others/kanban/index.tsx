7e0818f85c54d892917b174758cece80209c1eae
liaoyi <2417276459@qq.com>
2023-12-26 16:57:51 +0800

    feat: 看板页列拖动功能(从当前列拖动到另一列) :sparkles:

--- src/pages/sys/others/kanban/index.tsx
+++ src/pages/sys/others/kanban/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { DragDropContext, OnDragEndResponder } from 'react-beautiful-dnd';
-
+import SimpleBar from 'simplebar-react';
 import KanbanColumn from '@/pages/sys/others/kanban/kanban-column';
 import { Column, DndDataType } from '@/pages/sys/others/kanban/types';
 
@@ -15,26 +15,48 @@ export default function Kanban() {
     if (!destination) {
       return;
     }
-
     if (destination.droppableId === source.droppableId && destination.index === source.index) {
       return;
     }
 
-    const column = state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const start = state.columns[source.droppableId];
+    const finish = state.columns[destination.droppableId];
 
-    const newColumn: Column = {
-      ...column,
-      taskIds: newTaskIds,
-    };
+    // moving task in one column
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn: Column = { ...start, taskIds: newTaskIds };
+
+      const newState: DndDataType = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setState(newState);
+      return;
+    }
+
+    // moving task from on column to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart: Column = { ...start, taskIds: startTaskIds };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish: Column = { ...finish, taskIds: finishTaskIds };
 
     const newState: DndDataType = {
       ...state,
       columns: {
         ...state.columns,
-        [newColumn.id]: newColumn,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
       },
     };
 
@@ -43,12 +65,15 @@ export default function Kanban() {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      {state.columnOrder.map((columnId) => {
-        const column = state.columns[columnId];
-        const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
-
-        return <KanbanColumn key={columnId} column={column} tasks={tasks} />;
-      })}
+      <SimpleBar className="h-full">
+        <div className="flex h-full items-start gap-6 p-1">
+          {state.columnOrder.map((columnId) => {
+            const column = state.columns[columnId];
+            const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+            return <KanbanColumn key={columnId} column={column} tasks={tasks} />;
+          })}
+        </div>
+      </SimpleBar>
     </DragDropContext>
   );
 }
--- src/pages/sys/others/kanban/kanban-column.tsx
+++ src/pages/sys/others/kanban/kanban-column.tsx
@@ -1,31 +1,33 @@
+import { Button } from 'antd';
 import { CSSProperties } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 
+import { Iconify } from '@/components/icon';
 import KanbanTask from '@/pages/sys/others/kanban/kanban-task';
 import { Column, Task } from '@/pages/sys/others/kanban/types';
 import { useSettings } from '@/store/settingStore';
 
 import { ThemeMode } from '#/enum';
 
-type Props = {
-  column: Column;
-  tasks: Task[];
-};
+type Props = { column: Column; tasks: Task[] };
+
 export default function KanbanColumn({ column, tasks }: Props) {
   const { themeMode } = useSettings();
 
   const style: CSSProperties = {
+    height: '100%',
     padding: '16px',
     borderRadius: '16px',
     backgroundColor:
       themeMode === ThemeMode.Light ? 'rgb(244, 246, 248)' : 'rgba(145, 158, 171, 0.12)',
   };
+
   return (
     <div style={style}>
       <header className="my-4 text-xl font-bold">{column.title}</header>
       <Droppable droppableId={column.id}>
         {(provided) => (
-          <main ref={provided.innerRef} {...provided.droppableProps}>
+          <main ref={provided.innerRef} {...provided.droppableProps} className="min-h-[10px]">
             {tasks.map((task, index) => (
               <KanbanTask task={task} key={task.id} index={index} />
             ))}
@@ -33,6 +35,17 @@ export default function KanbanColumn({ column, tasks }: Props) {
           </main>
         )}
       </Droppable>
+      <footer className="w-72">
+        <Button
+          className="flex items-center"
+          type="text"
+          block
+          size="large"
+          icon={<Iconify icon="carbon:add" size={20} />}
+        >
+          Add Task
+        </Button>
+      </footer>
     </div>
   );
 }
